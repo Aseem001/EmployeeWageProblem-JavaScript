34144176e5f98db0df59613c3814e72c9aa8d530
Aseem001 <aseemanand2@gmail.com>
2020-11-16 00:27:32 +0530

    [Aseem] Added code to calculate wage umder working hr and days constraints

--- EmployeeWageUsingJS.js
+++ EmployeeWageUsingJS.js
@@ -1,7 +1,7 @@
 console.log("Welcome to employee wage problem\n");
 
 //UC 1 : Check if employee is present or absent
-console.log("Checking if employee is present or absent:");
+console.log("UC 2\nChecking if employee is present or absent:");
 const IS_ABSENT = 0;
 //UC 2 Refactor
 let empCheck = Math.floor(Math.random() * 10) % 3;
@@ -15,7 +15,7 @@ let empCheck = Math.floor(Math.random() * 10) % 3;
 }
 
 //UC 2 : Calculate daily wage based on part time or full time
-console.log("Calculating wage for employee:");
+console.log("UC 3\nCalculating wage for employee:");
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -40,6 +40,7 @@ let empWage = WAGE_PER_HOUR*getWorkingHrs(empCheck);
 console.log("Employee wage for the day: " + empWage);
 
 // UC 4 : Calculate total emp wage for a month assuming 20 working days
+console.log("UC 4\nCalculating monthly wage given 20 days")
 const NUM_OF_WORKING_DAYS = 20;
 empHrs=0;
 for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
@@ -49,3 +50,23 @@ for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
 }
 empWage=empHrs*WAGE_PER_HOUR;
 console.log("Total monthly wage: "+empWage);
+
+//UC 5 : Calculate wages till a condition of total working hours of 160 or max days of 20 is reached for a month
+console.log("UC 5\nCalculating monthly wage given condition")
+const MAX_HOURS_IN_A_MONTH=160;
+const MAX_NUMBER_OF_DAYS=20;
+let totalWorkHrs=0;
+let totalWorkingDays=0;
+while(totalWorkingDays<MAX_NUMBER_OF_DAYS&& totalWorkHrs<MAX_HOURS_IN_A_MONTH)
+{
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs=getWorkingHrs(empCheck);
+    if(empHrs!=0 && totalWorkHrs==156)
+    {
+        empHrs=PART_TIME_HOURS;
+    }
+    totalWorkHrs+=empHrs;
+}
+let totalEmpWage=totalWorkHrs*WAGE_PER_HOUR;
+console.log("UC 5\nTotal days worked: "+totalWorkingDays+"\nTotal work hours: "+totalWorkHrs+"\nTotal monthly wage: "+totalEmpWage);
