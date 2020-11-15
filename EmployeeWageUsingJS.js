2110b40ccf302e4721061ae54cf1ad593a0c37df
Aseem001 <aseemanand2@gmail.com>
2020-11-15 23:15:33 +0530

    [Aseem].[Refactor] code to calculate emp wage using function

--- EmployeeWageUsingJS.js
+++ EmployeeWageUsingJS.js
@@ -1,7 +1,9 @@
-console.log("Welcome to employee wage problem");
+console.log("Welcome to employee wage problem\n");
+
 //UC 1 : Check if employee is present or absent
-console.log("UC1: Checking if employee is present or absent");
+console.log("Checking if employee is present or absent:");
 const IS_ABSENT = 0;
+//UC 2 Refactor
 let empCheck = Math.floor(Math.random() * 10) % 3;
 {
     if (empCheck == IS_ABSENT) {
@@ -11,27 +13,31 @@ let empCheck = Math.floor(Math.random() * 10) % 3;
         console.log("Employee is Present");
     }
 }
+
 //UC 2 : Calculate daily wage based on part time or full time
-console.log("UC2: Calculating wage for employee");
+console.log("Calculating wage for employee:");
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 let empHrs = 0;
-switch (empCheck) 
+
+// UC 3 Refactor
+function getWorkingHrs(empCheck)
 {
-    case IS_PART_TIME:
-        console.log("Employee is working part time");
-        empHrs = 4;
-        break;
-    case IS_FULL_TIME:
-        console.log("employee is working full time");
-        empHrs = 8;
-        break;
-    default:       
-        empHrs = 0;
-        break;
+    switch (empCheck) 
+    {
+        case IS_PART_TIME:
+            console.log("Employee is working part time");
+            return PART_TIME_HOURS;          
+        case IS_FULL_TIME:
+            console.log("employee is working full time");
+            return FULL_TIME_HOURS;
+        default:       
+            return 0;
+    }
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee wage: " + empWage);
+
+let empWage = WAGE_PER_HOUR*getWorkingHrs(empCheck);
+console.log("Employee wage: " + empWage+"\n");
